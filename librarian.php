13613cad493b1b60aad17e858fcc7944dbfbb9ed
Tobias Alexander Franke <tobias.franke@siggraph.org>
2024-04-27 16:54:36 +0200

    General overhaul, use double hash for user feeds

--- librarian.php
+++ librarian.php
@@ -6,6 +6,13 @@
      *
      */
 
+    // Global configuration
+    $max_items = 100;
+    $url_base = 'http' . (isset($_SERVER['HTTPS']) ? '' : '') . '://' . $_SERVER['HTTP_HOST'];
+    $url_librarian = $url_base . $_SERVER["PHP_SELF"];
+    $dir_subs = "feeds";
+    
+    // Fetch parameters given to librarian
     function fetch_param($param)
     {
         $params = $_GET;
@@ -19,36 +26,38 @@
         return "";
     }
 
-    $max_items = 100;
-    $url_base = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'];
-    $url_librarian = $url_base . $_SERVER["PHP_SELF"];
-    $param_url = fetch_param("url");
-    $param_id = fetch_param("id");
-    $dir_subs = "subscriptions";
-    $local_rssfile = "";
-    
-    if ($param_id)
-        $local_rssfile = $dir_subs . "/" . $param_id . ".xml";
-
-    function update_rss_file()
+    // Turn user ID into feed ID
+    function get_feed_id($user_id)
     {
-        global $local_rssfile;
+        return hash('sha256', $user_id);
+    }
+
+    // Produce path for local feed file
+    function get_local_feed_file($param_id)
+    {
+        global $dir_subs;
+        $file_hash = get_feed_id($param_id);
+        return $dir_subs . "/" . $file_hash . ".xml";
+    }
+
+    // Update feed files with new header
+    function update_feed_file($user_id)
+    {
         global $dir_subs;
         global $url_base;
         global $url_librarian;
-        global $param_id;
 
         // check for subs dir
         if (!is_dir($dir_subs))
             mkdir($dir_subs);
 
-        $personal_url = $url_librarian . '?id=' . $param_id;
+        $personal_url = $url_librarian . '?id=' . $user_id;
 
         // recreate base file so changes in the header are put in with every new release
         $new_rss_base_text = '<?xml version="1.0" encoding="utf-8"?>
         <rss version="2.0">
             <channel>
-                <title>RSS-Librarian</title>
+                <title>RSS-Librarian (' . substr($user_id, 0, 4) . ')</title>
                 <description>A read-it-later service for RSS purists</description>
                 <link>' . $personal_url . '</link>
             </channel>
@@ -56,12 +65,13 @@
         ';
 
         $rss_xml = simplexml_load_string($new_rss_base_text);
+        $local_feed_file = get_local_feed_file($user_id);
 
         // try to open local subscriptions and copy items over
-        $local_rsstext = @file_get_contents($local_rssfile);
-        if ($local_rsstext != "") 
+        $local_feed_text = @file_get_contents($local_feed_file);
+        if ($local_feed_text != "") 
         {
-            $old_rss_xml = simplexml_load_string($local_rsstext);
+            $old_rss_xml = simplexml_load_string($local_feed_text);
             
             foreach($old_rss_xml->channel->item as $item)
                 sxml_append($rss_xml->channel, $item);
@@ -70,6 +80,7 @@
         return $rss_xml;
     }
 
+    // Helper to attach new XML element to existing one
     function sxml_append(SimpleXMLElement $to, SimpleXMLElement $from) 
     {
         $toDom = dom_import_simplexml($to);
@@ -79,38 +90,30 @@
         $toDom->insertBefore($new_node, $firstSibling);
     }
 
-    function fetch_rss_item($url)
+    // Turn URL article into a readable feed item
+    function fetch_feed_item($url)
     {
-        $rssurl = "https://ftr.fivefilters.net/makefulltextfeed.php?url=" . urlencode($url);
+        $feed_url = "https://ftr.fivefilters.net/makefulltextfeed.php?url=" . urlencode($url);
         
-        $rsstext = file_get_contents($rssurl);
+        $feed_item = file_get_contents($feed_url);
 
         // error handling remove everything until first <
-        $start = strpos($rsstext, "<");
-        $rsstext = substr($rsstext, $start);
-        $xml = simplexml_load_string($rsstext);
+        $start = strpos($feed_item, "<");
+        $feed_item = substr($feed_item, $start);
+        $xml = simplexml_load_string($feed_item);
         $oneitem = $xml->channel->item[0];
         return $oneitem;
     }
 
-    function add_url()
+    // Add URL to personal feed
+    function add_url($user_id, $param_url)
     {
-        global $param_id;
-        global $param_url;
-        global $local_rssfile;
         global $max_items;
         global $dir_subs;
 
-        // generate id if none is there
-        $local_rsstext = "";
-        if ($param_id == "")
-        {
-            $param_id = hash('sha256', random_bytes(18));
-        }
-
-        $local_rssfile = $dir_subs . "/" . $param_id . ".xml";
+        $local_feed_file = get_local_feed_file($user_id);
         
-        $xml = update_rss_file();
+        $xml = update_feed_file($user_id);
         
         if ($xml->channel->item)
         {
@@ -131,16 +134,17 @@
         }
 
         // fetch rss content and add
-        $item = fetch_rss_item($param_url);
+        $item = fetch_feed_item($param_url);
         $pub_date = date("D, d M Y H:i:s T");
         $item->addChild("pubDate", $pub_date);
         sxml_append($xml->channel, $item);
 
         // write to rss file
-        file_put_contents($local_rssfile, $xml->asXml());
+        file_put_contents($local_feed_file, $xml->asXml());
         return '<a href="' . $param_url . '">' . $param_url . '</a> added';
     }
 
+    // Count number of feeds in feed directory
     function count_feeds()
     {
         global $dir_subs;
@@ -154,9 +158,17 @@
 <head>
   <title>RSS-Librarian</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="shortcut icon" href="favicon.png">
   <?php
+    $param_url = fetch_param("url");
+    $param_id = fetch_param("id");
+    $user_id = $param_id;
+
+    // user exists?
     if ($param_id != "") 
-        print('<link rel="alternate" type="application/rss+xml" title="Personal feed (' . substr($param_id, 0, 4) . ')" href="' . $local_rssfile . '">');
+        print('<link rel="alternate" type="application/rss+xml" title="Personal feed (' . substr($param_id, 0, 4) . ')" href="' . get_local_feed_file($param_id) . '">');
+    else
+        $user_id = hash('sha256', random_bytes(18));
   ?>
 
   <style>
@@ -218,18 +230,21 @@
         </div>
 <?php
     $add_id = "";
+
+    // Check if parameter was supplied to distinguish new users from existing ones
     if ($param_id != "") 
     {
-        print_r('Subscribe to <a href="' . $local_rssfile . '">personal feed</a> (' . substr($param_id, 0, 4) . '), preview <a href="https://feedreader.xyz/?url=' . urlencode($url_base . '/' . $local_rssfile) . '">it here</a><br><br>');
-        $add_id = '<input type="hidden" id="id" name="id" value="' . $param_id . '">';
+        $local_feed_file = get_local_feed_file($user_id);
+        print_r('Subscribe to your <a href="' . $local_feed_file . '">personal feed</a> (' . substr($user_id, 0, 4) . '), preview <a href="https://feedreader.xyz/?url=' . urlencode($url_base . '/' . $local_feed_file) . '">it here</a><br><br>');
+        $add_id = '<input type="hidden" id="id" name="id" value="' . $user_id . '">';
     }
 
-    print_r('Paste a new URL here:<br><form action="' . $url_librarian . '"><input type="text" id="url" name="url">'.$add_id.'<br><input type="submit" value="Add to feed"></form><br><br>');
+    print_r('Paste a new URL here:<br><form action="' . $url_librarian . '"><input type="text" id="url" name="url">' . $add_id . '<br><input type="submit" value="Add to feed"></form><br><br>');
     
     if ($param_url != "")
     {
-        $result = add_url();
-        $personal_url = $url_librarian . '?id=' . $param_id;
+        $result = add_url($user_id, $param_url);
+        $personal_url = $url_librarian . '?id=' . $user_id;
 
         print_r($result . "<br><br>");
         print_r('Use <a href="javascript:window.location.href=\'' . $personal_url . '&url=\' + window.location.href">this boomarklet</a> to add the current open page<br><br>');
