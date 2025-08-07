512d9507499613cfc09889a02bdb24a926a84aa0
Tobias Alexander Franke <tobias.franke@siggraph.org>
2025-08-07 14:27:44 +0200

    Add welcome message for new users

--- librarian.php
+++ librarian.php
@@ -560,8 +560,9 @@
         print('
             <h2>Your feed</h2>
             <p>
-                Your <a href="'. $personal_url .'">personal URL</a> and 
-                <a href="' . $feed_url . '">personal feed</a>
+                Bookmark your <a href="'. $personal_url .'">personal URL</a><br> 
+                Subscribe to your <a href="' . $feed_url . '">personal RSS/Atom feed</a> with a reader<br><br>
+                <a href="' . $feed_url . '"><svg xmlns="http://www.w3.org/2000/svg" style="width: 1em" fill="currentColor" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path><path d="M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1-1-1m0 4a1 1 0 0 1 1-1 6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1-1-1"></path></svg></a>
             </p>
 
             <h2>Your tools</h2>
@@ -571,7 +572,18 @@
                 <a href="javascript:window.location.href=\'' . $personal_url . '&url=\' + window.location.href">Feed boomarklet</a>, 
                 <a href="https://www.icloud.com/shortcuts/d047b96550114317beb45bb57466a88f">iOS Shortcut</a>
             </p>');
-    
+        else
+        print('
+            <h2>What Is This?</h2>
+            <p>
+                <a href="https://github.com/thefranke/rss-librarian/wiki#how-to-use">Read the wiki!</a><br><br>
+                RSS-Librarian is a read-it-later service. You can store individual articles in your own personal RSS/Atom feed
+                and read them later with a feed reader app. Simpliy add a URL you want to save, store the personal URL and
+                subscribe to the personal feed that will be created for you. With the personal URL you can later add more
+                articles to read later.
+            </p>
+        ');
+
         print('
             <h2>Feed Readers</h2>
             <p>  
