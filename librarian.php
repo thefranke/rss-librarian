a1561b6f893ec6e7703efde0d9b9707827a5d961
Tobias Alexander Franke <tobias.franke@siggraph.org>
2026-07-15 22:27:57 +0200

    Upgrade user agent

--- librarian.php
+++ librarian.php
@@ -379,7 +379,7 @@
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
+            CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0',
             CURLOPT_ENCODING       => '',
             CURLOPT_COOKIEFILE     => $cookie_file,
             CURLOPT_COOKIEJAR      => $cookie_file,
