95da4fc4b233cfa4faa7c5553698c589e1832616
Tobias Alexander Franke <tobias.franke@siggraph.org>
2026-08-30 11:19:26 +0200

    Report installed modules

--- librarian.php
+++ librarian.php
@@ -745,7 +745,7 @@
                 Feed format: ' . ($g_config['use_rss_format'] ? 'RSS 2.0' : 'Atom') . '<br>
                 Version: ' .  get_version() . '<br>' .
                 ((!empty($g_config['instance_contact'])) ? 'Contact: ' . $g_config['instance_contact'] . '<br>': '') . 
-                '<a href="https://github.com/thefranke/rss-librarian/issues/new?body=Your%20description%20here...%0A%0A%60%60%60%0AFTE: ' . $g_config['extract_content'] . '%0AUseRSS: ' . $g_config['use_rss_format'] . '%0AMaxItems: ' . $g_config['max_items'] . '%0AVersion: ' . get_version() . '%60%60%60">Open a Github Issue</a><br>
+                '<a href="https://github.com/thefranke/rss-librarian/issues/new?body=Your%20description%20here...%0A%0A%60%60%60%0AFTE: ' . $g_config['extract_content'] . '%0AReadability: ' . file_exists($g_config['mod_readability']) . '%0ACustomextractor: ' . file_exists($g_config['mod_readability']) . '%0AUseRSS: ' . $g_config['use_rss_format'] . '%0AMaxItems: ' . $g_config['max_items'] . '%0AVersion: ' . get_version() . '%0A%60%60%60">Open a Github Issue</a><br>
             </p>
             ' . ((!empty($param_id)) ? '<p><a href="' . $feed_url . '"><svg xmlns="http://www.w3.org/2000/svg" style="width: 2em" fill="currentColor" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"></path><path d="M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-3-8.5a1 1 0 0 1 1-1c5.523 0 10 4.477 10 10a1 1 0 1 1-2 0 8 8 0 0 0-8-8 1 1 0 0 1-1-1m0 4a1 1 0 0 1 1-1 6 6 0 0 1 6 6 1 1 0 1 1-2 0 4 4 0 0 0-4-4 1 1 0 0 1-1-1"></path></svg></a></p>': '') . '
         </footer>');
